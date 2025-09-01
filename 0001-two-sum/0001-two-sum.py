53ba9b9a1d34149041fdce7a704025b9b8a8fa44
Gowrima <kj.gowrima@gmail.com>
2025-09-01 10:06:16 -0700

    Time: 4 ms (42.3%), Space: 19.5 MB (5.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,17 +2,22 @@ from collections import defaultdict
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_ = defaultdict()
-        result = tuple()
+        #Construct the hash map between nums and their indices
+        map = defaultdict()
 
         for i in range(len(nums)):
-            to_find = target-nums[i]
+            if (not nums[i] in map):
+                map[nums[i]] = [i]
+            else:
+                map[nums[i]].append(i)
 
-            if to_find in hash_:
-                result = (i, hash_[to_find])
-                break
-            
-            hash_[nums[i]] = i
-        
-        return result
-        
+        # Go through the elements of nums, and find its corresponding index pair
+
+        for i in range(len(nums)):
+            if (target - nums[i] in map):
+                if (len(map[target - nums[i]]) > 1 and target == nums[i]*2):
+                    return map[nums[i]][:2]
+                elif (len(map[target - nums[i]]) == 1 and target == nums[i]*2):
+                    continue
+                else:
+                    return [i, map[target - nums[i]][0]]
