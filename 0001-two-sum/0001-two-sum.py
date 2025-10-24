efc42a6407f0b2cd5471029e6fb00649bb2af742
Gowrima <kj.gowrima@gmail.com>
2025-10-24 14:02:45 -0700

    Time: 0 ms (100%), Space: 19.2 MB (10.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,16 @@
+from collections import defaultdict
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = defaultdict()
-        result = tuple()
+        hash_ = defaultdict()
+        res = tuple()
 
         for i in range(len(nums)):
-            to_find = target - nums[i]
-
-            if to_find in map:
-                result = (map[to_find], i)
+            to_find = target - nums[i]       
+            
+            if to_find in hash_:
+                res = (hash_[to_find], i)
             else:
-                map[nums[i]] = i
-        
-        return result
+                hash_[nums[i]] = i
+
+        return res
