ff67da86442ee353e0ab644460a6fb4a5a5881fd
Gowrima <kj.gowrima@gmail.com>
2025-10-30 12:23:51 -0700

    Time: 455 ms (86.36%), Space: 20.8 MB (20.3%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,33 @@
 class Solution:
+    def twoSum(self, nums, i, res):
+        lo, hi = i+1, len(nums)-1
+
+        while lo<hi:
+            sum_ = nums[i]+nums[lo]+nums[hi]
+
+            if sum_ == 0:
+                res.append([nums[i], nums[lo], nums[hi]])
+                lo += 1
+                hi -= 1
+
+                while lo<hi and nums[lo] == nums[lo-1]:
+                    lo += 1
+
+            elif sum_ < 0:
+                lo += 1
+            else:
+                hi -= 1
+
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        result = []
+        res = []
 
-        for i in range(len(nums)-2):
-            if i>0 and nums[i] == nums[i-1]:
-                continue
-
-            left = i+1
-            right = len(nums)-1
-
-            while left<right:
-                tmpSum = nums[i]+nums[left]+nums[right]
-
-                if tmpSum == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-
-                    while left<right and nums[left] == nums[left+1]:
-                        left += 1
-                    
-                    while left<right and nums[right] == nums[right-1]:
-                        right -= 1
-                    
-                    left += 1
-                    right -= 1
-                elif tmpSum < 0:
-                    left += 1
-                else:
-                    right -= 1
-
-        return result
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            
+            if i == 0 or nums[i-1] != nums[i]:
+                self.twoSum(nums, i, res)
+        
+        return res
+    
