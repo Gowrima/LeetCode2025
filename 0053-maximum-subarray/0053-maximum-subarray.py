c3a49127ad6f50a52c93915cb69356dc36d8c30e
Gowrima <kj.gowrima@gmail.com>
2025-11-13 19:06:46 -0800

    Time: 15 ms (97.96%), Space: 32.1 MB (66.36%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,9 +1,5 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
-        if len(nums) == 1:
-            return nums[0]
-
         cur_sum, max_sum = float('-inf'), float('-inf')
 
         for i in range(len(nums)):
@@ -11,8 +7,8 @@ class Solution:
                 cur_sum += nums[i]
             else:
                 cur_sum = nums[i]
-
+            
             if cur_sum > max_sum:
                 max_sum = cur_sum
-            
+
         return max_sum
