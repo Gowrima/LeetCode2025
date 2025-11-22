d52cd44ebcf6ff0e370d5c9e7b949329705ef8cd
Gowrima <kj.gowrima@gmail.com>
2025-11-22 12:04:59 -0800

    Time: 79 ms (48.9%), Space: 33.6 MB (81.32%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
@@ -7,13 +7,11 @@ class Solution:
 
         for i in range(len(players)):
             while j<len(trainers):
-                #print ("i=", i, "j =", j)
-
+                
                 if players[i] <= trainers[j]:
                     result += 1
                     j += 1
                     break
-                else:
-                    j += 1
+                j += 1
 
         return result
