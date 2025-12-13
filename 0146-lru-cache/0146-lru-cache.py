24e761903f2168097f578082871f6cb063de141a
Gowrima <kj.gowrima@gmail.com>
2025-12-13 11:01:39 -0800

    Time: 92 ms (95.3%), Space: 77.7 MB (97.1%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -12,7 +12,7 @@ class LRUCache:
         if key in self.cache:
             res = self.cache[key]
             self.cache.move_to_end(key, last=True)
-
+        
         return res
 
     def put(self, key: int, value: int) -> None:
@@ -20,15 +20,14 @@ class LRUCache:
             self.cache[key] = value
             self.cache.move_to_end(key, last=True)
             return
-        
+
         if len(self.cache) >= self.capacity:
-            # evict
+            # evict least recently used
             self.cache.popitem(last=False)
-        
+
         self.cache[key] = value
         self.cache.move_to_end(key, last=True)
-
-
+        
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
