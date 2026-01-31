270094752603f1392654159fb19c4d3c161dc96c
Gowrima <kj.gowrima@gmail.com>
2026-01-31 11:23:05 -0800

    Time: 0 ms (100%), Space: 44.3 MB (68.72%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -1,24 +1,30 @@
 # Definition for a binary tree node.
-class TreeNode:
-     def __init__(self, val=0, left=None, right=None):
-         self.val = val
-         self.left = left
-         self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
 
 class Solution:
-    def minDepth(self, root: TreeNode) -> int:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
 
-        ldepth = self.minDepth(root.left)
-        rdepth = self.minDepth(root.right)
+        queue = deque()
+        queue.append(root)
+        depth = 1
 
-        if root.left is None and root.right is None:
-            return 1
+        while queue:
+            for i in range(len(queue)):
+                curr_node = queue.popleft()
+                if curr_node.left is not None:
+                    queue.append(curr_node.left)
+                if curr_node.right is not None:
+                    queue.append(curr_node.right)
+                if curr_node.left is None and curr_node.right is None:
+                    return depth
+            depth += 1
 
-        if root.left is None:
-            return 1+rdepth
-        elif root.right is None:
-            return 1+ldepth
-        
-        return min(ldepth, rdepth)+1
+        return depth
