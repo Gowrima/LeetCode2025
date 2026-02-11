5cb2e5932f888ac9413215ef9d543350c017013c
Gowrima <kj.gowrima@gmail.com>
2026-02-11 09:12:38 -0800

    Time: 3 ms (73.82%), Space: 21.3 MB (59.85%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        # [2,4], [2,3]
+        intervals.sort()
+
+        for i in range(len(intervals)-1):
+            if intervals[i+1][0] < intervals[i][1]:
+                return False
+            
+        return True
