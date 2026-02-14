c6289062ee5891e19cbb9ab41e025fb01b092427
Gowrima <kj.gowrima@gmail.com>
2026-02-14 11:05:06 -0800

    Time: 11 ms (27.45%), Space: 22.4 MB (66.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -7,7 +7,7 @@ class Solution:
         intervals.sort()
         
         result = [intervals[0]]  # [1,8]
-        i, j = 0, 1
+        j = 1
 
         # [1,8],[2,6],[7,10]
         #
@@ -18,17 +18,16 @@ class Solution:
         while j < len(intervals):
             # comapre result with intervals[j]
             # [1,6], [8,10]
-            if intervals[j][0] <= result[i][1]:
+            if intervals[j][0] <= result[-1][1]:
                 new_interval = [
-                    min(intervals[j][0], result[i][0]),
-                    max(intervals[j][1], result[i][1]),
+                    min(intervals[j][0], result[-1][0]),
+                    max(intervals[j][1], result[-1][1]),
                 ]
                 # [1,8]
-                del result[i]
+                result.pop()
                 result.append(new_interval)  # [1,10]
             else:
                 result.append(intervals[j])
-                i += 1
 
             j += 1  # 2,
 
