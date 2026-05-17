3c07894fdf60f0a57c42a81261e6640e91db8849
Gowrima <kj.gowrima@gmail.com>
2026-05-17 15:38:00 -0700

    Time: 110 ms (74.16%), Space: 76.7 MB (85.28%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -6,15 +6,17 @@ class LRUCache:
         self.cache = OrderedDict()
         self.capacity = capacity
 
+
     def get(self, key: int) -> int:
-        res = -1
+        result = -1
 
         if key in self.cache:
-            res = self.cache[key]
+            result = self.cache[key]
             self.cache.move_to_end(key, last=True)
         
-        return res
+        return result
 
+    # cache = [(2,6), (1,2)]
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.cache[key] = value
@@ -22,14 +24,36 @@ class LRUCache:
             return
 
         if len(self.cache) >= self.capacity:
-            # evict least recently used
+            # delete least recent at 0
             self.cache.popitem(last=False)
-
+        
         self.cache[key] = value
         self.cache.move_to_end(key, last=True)
-        
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+
+'''
+capacity = 3
+put(5, 10)
+put(6, 20)
+put(7, 30)
+get(6)
+put(1, 40)
+
+map = {}  # dict[Key: int, Node (pointer to node in Deque)]
+{
+
+    6: Node(20)
+    7: Node(30)
+}
+
+dq = deque()  # value
+ <-> Node(30, 7) <-> Node(20, 6)
+
+
+'''
