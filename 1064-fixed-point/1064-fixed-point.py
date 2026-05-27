227164df454e8c9c5b9a1d0a9878c35246c989bd
Gowrima <kj.gowrima@gmail.com>
2026-05-26 17:35:02 -0700

    Time: 0 ms (100%), Space: 20.1 MB (96.41%) - LeetHub

--- 1064-fixed-point/1064-fixed-point.py
+++ 1064-fixed-point/1064-fixed-point.py
@@ -1,48 +1,18 @@
 class Solution:
     def fixedPoint(self, arr: List[int]) -> int:
-        # expected_value = 0
-        # for idx, num in enumerate(arr):
-        #     if num == expected_value:
-        #         return idx
-        #     expected_value += 1
-        # return -1
+        result = -1
+        left, right = 0, len(arr)-1
 
-        left = 0
-        right = len(arr) - 1
+        while left<=right:
+            mid = (right+left)//2
 
-        ans = float("inf")
-        while left <= right:
-            mid = left + (right - left) // 2
-            if mid == arr[mid]:
-                ans = min(ans, mid)
-                right = mid - 1
-
-            # condition 1: search left
-            elif mid < arr[mid]:
-                right = mid - 1
-
-            # condition 2: search right
-            # else: mid < arr[mid]:
+            if arr[mid] == mid:
+                result = mid
+                right = mid-1
+            elif arr[mid] < mid:
+                left = mid+1
             else:
-                left = mid + 1
-
-        if ans == float("inf"):
-            return -1
-        return ans
-
-
-"""
-  0   1  2  3  4
--10, -5, 0, 3, 7
-
-
-  0   1  2  3  4
--10,  1, 3, 3, 7
-
-  0   1  2  3  4
--10, -5, 3, 3, 7
-
-
-0 1 2 3 4
-0 1 2 3 4
-"""
+                right = mid-1
+        
+        return result
+        
